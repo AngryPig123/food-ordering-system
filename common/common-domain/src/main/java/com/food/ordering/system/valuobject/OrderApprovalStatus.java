c806353a1a69451e5e1a9b25428d22d1e8d73a10
Angrypig <shugddnr2@naver.com>
2026-03-10 18:01:59 +0900

    https://www.udemy.com/course/microservices-clean-architecture-ddd-saga-outbox-kafka-kubernetes-k/learn/lecture/42194638?start=195#overview

---
+++ common/common-domain/src/main/java/com/food/ordering/system/valuobject/OrderApprovalStatus.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.valuobject;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderApprovalStatus {
+    APPROVED, REJECTED
+}
---
+++ common/common-domain/src/main/java/com/food/ordering/system/valuobject/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.food.ordering.system.valuobject;
+
+public enum PaymentStatus {
+    COMPLETED, CANCELLED, FAILED
+}
